b9f7fd2f026bd3d884bd8c7c9ae5a9bc16f5750e
demchukv <demchukv@gmail.com>
2023-10-17 18:06:37 +0300

    ready

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,14 @@ console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will 
 Виклик getShippingMessage("Sweden", 100, 20) повертає "Shipping to Sweden will cost 120 credits"
 Виклик getShippingMessage з будь якими-валідними аргументами повертає правильне значення
 */
+
+'use strict';
+
+function getShippingMessage(country, price, deliveryFee) {
+    const totalPrice = price + deliveryFee;
+    return `Shipping to ${country} will cost ${totalPrice} credits`;
+}
+
+console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
